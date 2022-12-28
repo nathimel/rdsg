5d182c5826b1be1602c73af9d7599fe000d0e029
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-12-28 12:27:16 -0800

    much higher complexity than counterparts

--- src/curve.py
+++ src/curve.py
@@ -18,6 +18,14 @@ def main(config):
     # save one curve for multiple analyses
     game_dir = os.getcwd().replace(config.filepaths.simulation_subdir, "")
     curve_fn = os.path.join(game_dir, config.filepaths.curve_points_save_fn)    
+    counterparts_fn = os.path.join(game_dir, config.filepaths.counterpart_points_fn)
+
+    ba = lambda betas: get_curve_points(
+        game_params["prior"],
+        game_params["dist_mat"],
+        betas,
+        unique=True,
+    )
 
     # B-A gets a bit sparse in low-rate regions for np.linspace
     betas = np.concatenate([
@@ -25,12 +33,7 @@ def main(config):
         np.linspace(start=0.3, stop=0.9, num=33),
         np.linspace(start=1.0, stop=2**7, num=334),
     ])
-    points = get_curve_points(
-        game_params["prior"],
-        game_params["dist_mat"],
-        betas,
-        unique=True,
-    )
+    points = ba(betas)
     # curve must be interpolated before notebook analyses
     curve_data = interpolate_curve(util.points_to_df(points), max_distortion=30)
 
@@ -40,6 +43,14 @@ def main(config):
         )
     
     # TODO: use hydra to infer the list of swept alpha values to obtain beta-counterparts
+    alphas = np.array(range(0, 11, 2)).astype(float)
+    betas = alphas ** -2
+    # 0 ** -2 \to \infty, just use 1000
+    betas[0] = 1000
+    df = util.points_to_df(ba(betas))
+    df["beta"] = betas
+    df["alpha"] = alphas
+    util.save_points_df(counterparts_fn, df)
 
 if __name__ == "__main__":
     main()
