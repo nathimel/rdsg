f3fc9516a25ed406038757fe58d45e577806b3d1
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2023-01-08 18:06:16 -0800

    actually fuck seaborn

--- scripts/run_nos_normed_sweep.sh
+++ scripts/run_nos_normed_sweep.sh
@@ -12,12 +12,13 @@ ARGS=(
     "game/similarity=nosofsky_normed" 
     "game.distortion=squared_dist" 
     "simulation.num_trials=100"
-    "explore_space/pool_size=large"
-    "simulation.trajectory=True"
+    # "explore_space/pool_size=large"
+    # "simulation.trajectory=True"
+    "simulation.num_variants=1000"
     )
 
 echo python src/run_simulations.py -m $"${ARGS[@]}" $"${SWEEP[@]}" 
-python src/run_simulations.py -m $"${ARGS[@]}" $"${SWEEP[@]}" 
+time python src/run_simulations.py -m $"${ARGS[@]}" $"${SWEEP[@]}" 
 
 # echo python src/explore.py $"${ARGS[@]}"
 # python src/explore.py $"${ARGS[@]}"
