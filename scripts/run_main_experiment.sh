8ed7c304a059ae9b4f30e89df8eff285c2f7c9fa
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2023-07-18 14:05:25 -0700

    bug fix to expected_distortion in altk improves, not worsens, performance

--- scripts/run_main_experiment.sh
+++ scripts/run_main_experiment.sh
@@ -3,4 +3,4 @@
 # Runs main experiment (parameter sweeps and analyses) in paper
 
 echo "./run_nos_normed_sweep.sh"
-./run_nos_normed_sweep.sh
+./scripts/run_nos_normed_sweep.sh
--- scripts/run_nos_normed_sweep.sh
+++ scripts/run_nos_normed_sweep.sh
@@ -5,13 +5,15 @@
 
 SWEEP=(
     "game.similarity.param=0, 1, 2, 4, 6, 8, 16"
-    "simulation/dynamics=reinforcement_learning, replicator_dynamic"
+    # "simulation/dynamics=reinforcement_learning, replicator_dynamic"
+    "simulation/dynamics=replicator_dynamic"
     )
 
 ARGS=(
     "game/similarity=nosofsky_normed" 
     "game.distortion=squared_dist" 
-    "simulation.num_trials=100"
+    # "simulation.num_trials=100"
+    "simulation.num_trials=8"    
     # "explore_space/pool_size=large"
     # "simulation.trajectory=True"
     "simulation.num_variants=1000"
